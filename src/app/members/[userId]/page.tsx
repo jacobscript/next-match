45b54da48919c15459a9a97fe3177c6cbfa26711
Jacob Lantsman <jacob.lantsman@gmail.com>
2024-06-03 21:44:36 -0400

    End of section 7

--- src/app/members/[userId]/page.tsx
+++ src/app/members/[userId]/page.tsx
@@ -1,5 +1,5 @@
 import { getMemberByUserId } from '@/app/actions/memberActions'
-import { CardBody, CardHeader, Divider } from '@nextui-org/react'
+import CardInnerWrapper from '@/components/CardInnerWrapper'
 import { notFound } from 'next/navigation'
 
 export default async function MemberDetailedPage({
@@ -11,12 +11,6 @@ export default async function MemberDetailedPage({
   if (!member) return notFound()
 
   return (
-    <>
-      <CardHeader className='text-2xl font-semibold text-secondary'>
-        Profile
-      </CardHeader>
-      <Divider />
-      <CardBody>{member.description}</CardBody>
-    </>
+    <CardInnerWrapper header='Profile' body={<div>{member.description}</div>} />
   )
 }
